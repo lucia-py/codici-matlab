1f647fa1e5ecba770e5401d26cb95c1e24a45ec8
lucia-py <lucia.benedetti@studenti.unipd.it>
2025-05-16 11:38:13 +0200

    test.m

--- esempio.m
+++ esempio.m
@@ -5,9 +5,16 @@ q=3;
 T=cluster_tree(1:length(x),q);
 [Bs,Bc]=base_S(x,T,q);
 
+n=2;%numero basi da plottare
+figure(1)
+for k=1:n
+    plot(x,Bs(k,:));
+end
+
 dati=sin(2*pi*x);
 coeff=Bs*dati(:);
 ricostruzione=Bs'*coeff;
 
+figure(2)
 plot(x,dati,'o',x,ricostruzione,'x');
 legend('originale','ricostruito');
